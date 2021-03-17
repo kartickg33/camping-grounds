4ee73e95c1346b62d8b7a84faaf5e477932ff206
kartickg33 <68003347+kartickg33@users.noreply.github.com>
2021-03-18 04:00:30 +0530

    restructured routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,29 +1,17 @@
 const express = require('express');
 const router = express.Router({mergeParams:true});
 const { reviewSchema } = require('../schemas.js');
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
+const Campground = require('../models/campground');
+const Review = require('../models/review');
+const reviews = require('../controllers/reviews');
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
-const Review = require('../models/review.js');
-const Campground = require('../models/campground')
-const {validateReview,isLoggedIn} = require('../middleware')
 
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
-router.post('/',validateReview,isLoggedIn,catchAsync(async (req,res)=>{
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success','Created new Review!');
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
-router.delete('/:reviewId',isLoggedIn,catchAsync(async (req,res)=>{
-    const {id,reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success','Successfully deleted Review!');
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router;
